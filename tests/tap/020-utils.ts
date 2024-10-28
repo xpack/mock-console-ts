bda5d5da8deaf0c112d40348ac30476c82e858c8
Liviu Ionescu <ilg@livius.net>
2024-10-28 15:57:10 +0200

    tests/tap update to pass c8

--- tests/tap/020-utils.ts
+++ tests/tap/020-utils.ts
@@ -32,37 +32,43 @@ import { MockConsole, dumpLines } from '../../src/index.js'
 await test('dumpLines', (t) => {
   const mockConsole = new MockConsole()
 
+  t.equal(mockConsole.outLines.length, 0, 'outLines has no lines')
+
   // const globalConsole = console
 
   // Override the global console with the mock console.
   // eslint-disable-next-line no-global-assign
   console = mockConsole
 
-  const array1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven',
+  const array1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
     'eight', 'nine', 'ten']
   dumpLines(array1)
 
   // globalConsole.log(mockConsole.outLines)
 
-  t.equal(mockConsole.outLines.length, 11, 'outLines has 11 lines')
+  t.equal(mockConsole.outLines.length, 12, 'outLines has 12 lines')
   t.equal(mockConsole.outLines[0], '', 'start with empty line')
-  t.equal(mockConsole.outLines[1], '0: \'one\'', 'first line is one')
-  t.equal(mockConsole.outLines[10], '9: \'ten\'', 'tenth line is ten')
+  t.equal(mockConsole.outLines[1], '"zero", // 0', 'first line is zero')
+  t.equal(mockConsole.outLines[11], '"ten", // 10', 'last line is ten')
   t.equal(mockConsole.errLines.length, 0, 'errLines is empty')
 
   mockConsole.clear()
 
-  const array2 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven',
-    'eight', 'nine', 'ten', 'eleven']
+  const array2 = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+    'Eight', 'Nine', 'Ten', 'Eleven']
   dumpLines(array2)
 
   // globalConsole.log(mockConsole.outLines)
 
-  t.equal(mockConsole.outLines.length, 12, 'outLines has 11 lines')
+  t.equal(mockConsole.outLines.length, 13, 'outLines has 12 lines')
   t.equal(mockConsole.outLines[0], '', 'start with empty line')
-  t.equal(mockConsole.outLines[1], ' 0: \'one\'', 'first line is one')
-  t.equal(mockConsole.outLines[11], '10: \'eleven\'', 'eleventh line is eleven')
+  t.equal(mockConsole.outLines[1], '"Zero", // 0', 'first line is zero')
+  t.equal(mockConsole.outLines[12], '"Eleven", // 11', 'last line is ten')
   t.equal(mockConsole.errLines.length, 0, 'errLines is empty')
+
+  mockConsole.clear()
+  dumpLines()
+  t.equal(mockConsole.outLines.length, 0, 'outLines has no lines')
 
   t.end()
 })
