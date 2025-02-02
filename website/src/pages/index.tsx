207b0ec0491b36c69e5a3396a82786c3e05f7781
Liviu Ionescu <ilg@livius.net>
2025-02-02 20:00:00 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -30,7 +30,8 @@ function HomepageHeader() {
       <HeadTitle title="Welcome to mock-console - The xPack Mock Console!" />
       <div className="container">
         <Heading as="h1" className="hero__title">{siteConfig.title}</Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">{siteConfig.tagline}
+        </p>
         <div className={styles.installWithCopy}>
           <InstallWithCopy>npm install @xpack/mock-console@1.0.0</InstallWithCopy>
         </div>
